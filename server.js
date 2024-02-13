bdd7eb69d04e8d9ab355e5e18aa5554663823b27
officialbigo <official.bigo.acc@gmail.com>
2024-02-13 20:12:55 +0530

    first

--- server.js
+++ server.js
@@ -20,9 +20,14 @@ require("dotenv").config();
 connectDb();
 const port = process.env.PORT || 5000;
 app.use(express.json());
-app.use(`/api/contacts`, require("./routes/contactRoutes"));
-app.use(`/api/users`, require("./routes/userRoutes"));
-app.use(errorHandler);
+try {
+  app.use(`/api/contacts`, require("./routes/contactRoutes"));
+  app.use(`/api/users`, require("./routes/userRoutes"));
+  app.use(errorHandler);
+} catch (error) {
+  console.log(error);
+}
+
 app.listen(port, () => {
   console.log(`this is soo fun , I am in port ${port}`);
 });
