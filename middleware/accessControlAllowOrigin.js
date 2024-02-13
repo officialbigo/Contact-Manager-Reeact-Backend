cca99041e3c3a58b2ef5ed08324a9d48c4cb915d
officialbigo <official.bigo.acc@gmail.com>
2024-02-13 19:55:04 +0530

    first

--- middleware/accessControlAllowOrigin.js
+++ middleware/accessControlAllowOrigin.js
@@ -2,7 +2,7 @@ const accessControlAllowOrigin = (req, res, next) => {
   // Website you wish to allow to connect
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://trk-contact-manager.netlify.app/"
+    "https://trk-contact-manager.netlify.app"
   );
 
   // Request methods you wish to allow
