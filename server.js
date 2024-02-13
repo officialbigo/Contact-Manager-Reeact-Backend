dacb48180f50a9109749fbb510aa10810b879db3
officialbigo <official.bigo.acc@gmail.com>
2024-02-13 08:39:28 +0530

    first

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const app = express();
 //   ],
 // }
 
+app.get("/cors", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.send({ msg: "This has CORS enabled 🎈" });
+});
+
 const cors = require("cors");
 app.use(cors());
 
